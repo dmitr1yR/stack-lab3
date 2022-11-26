547d01f6a5255a9e67d12f5b0622776217fe0658
dmitr1yR <104106501+dmitr1yR@users.noreply.github.com>
2022-11-26 15:59:38 +0300

    1.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,13 @@
-#include "TTrBMatrix.h"
-#include "TSqMatrix.h"
-#include "TVector.h"
+#include <iostream>
+#include "TFormuls.h"
+#include "TStack.h"
 
-int main()
-{
-  TSqMatrix<int> mat1(2);
-  TSqMatrix<int> mat2(2);
-  TSqMatrix<int> mat3(2);
-  TVector<float> vec1(2);
-  TVector<float> vec2(2);
-  TVector<float> vec3(2);
-  std::cin >> mat1[0][0];
-  std::cin >> mat1[0][1];
-  std::cin >> mat1[1][0];
-  std::cin >> mat1[1][1];
-  std::cin >> mat2[0][0];
-  std::cin >> mat2[0][1];
-  std::cin >> mat2[1][0];
-  std::cin >> mat2[1][1];
-  std::cin >> vec1[0];
-  std::cin >> vec1[1];
-  std::cin >> vec2[0];
-  std::cin >> vec2[1];
-  mat3 = mat2 + mat1;
-  std::cout << mat3[0][0];
-  std::cout << mat3[0][1];
-  std::cout << mat3[1][0];
-  std::cout << mat3[1][1];
-  mat3 = mat2 * mat1;
-  std::cout << mat3[0][0];
-  std::cout << mat3[0][1];
-  std::cout << mat3[1][0];
-  std::cout << mat3[1][1];
-  mat3 = mat2 * 1;
-  std::cout << mat3[0][0];
-  std::cout << mat3[0][1];
-  std::cout << mat3[1][0];
-  std::cout << mat3[1][1];
-  mat3 = mat2 - mat1;
-  std::cout << mat3[0][0];
-  std::cout << mat3[0][1];
-  std::cout << mat3[1][0];
-  std::cout << mat3[1][1];
-  vec3 = vec2 + vec1;
-  std::cout << vec3[0];
-  std::cout << vec3[1];
-  vec3 = vec2 * 2;
-  std::cout << vec3[0];
-  std::cout << vec3[1];
-  vec3 = vec2 * vec1;
-  std::cout << vec3[0];
-  std::cout << vec3[1];
+int main() {
+  TFormuls a("(2+3)+4*7/2");
+  a.Process();
+  std::cout<<a.CheckFormuls()<<std::endl;
+  std::cout<<a.Calculate()<<std::endl;
+  std::cout<<a.GetPostfix();
   return 0;
 }
+
